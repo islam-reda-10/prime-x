5033f227baa7bb4db0d0f91041256907e6ff6dff
islam-reda-10 <islam.reda.258@gmail.com>
2026-01-20 17:11:28 +0200

    Push files for prime-x-site implementation on 2026-01-20 15:11:13

---
+++ prime-x-site/script.js
@@ -0,0 +1,19 @@
+// Subscription Data
+const plans = {
+  prime: [
+    {
+      duration: "1 Month",
+      price: "500 L.E",
+      features: [
+        "2 Days weekly",
+        "1 Invitation",
+        "1 InBody",
+      ],
+    },
+    // Add other plans as needed
+  ],
+};
+
+/** Functions */
+//...
+console.log("WARNING")
